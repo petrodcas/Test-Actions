aa53cc18e8a8e508798e107bbfb55cd0f4c58491
Peter Rodríguez Casado <petrodcas@gmail.com>
2024-04-27 23:34:25 +0200

    Added some sample files

---
+++ some-golang-code/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+	"os"
+	"bytes"
+	"io"
+)
+
+func TestMain(t *testing.T) {
+	// capture stdout
+  old_stdout := os.Stdout
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("Error creating pipe: %s", err)
+	}
+	os.Stdout = w
+	defer func() {
+		os.Stdout = old_stdout
+	}()
+	outChannel := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outChannel <- buf.String()
+	}()
+	main()
+	w.Close()
+	// compare stdout with expected output
+	out := <-outChannel
+	if out != "Hello, World!\n" {
+		t.Errorf("Expected 'Hello, World!', got '%s'", out)
+	}
+}
